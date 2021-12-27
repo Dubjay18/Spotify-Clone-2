2b69d63b5f69d22395126baf19a89ddfe569fc2f
Dub_jay <jejeniyi7@gmail.com>
2021-12-27 23:58:44 +0100

    Update _middleware.js

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,7 +4,8 @@ import { NextResponse } from "next/server";
 export async function middleware(req) {
   const token = await getToken({ req, secret: process.env.JWT_SECRET });
   const { pathname } = req.nextUrl;
-
+console.log(pathname);
+  console.log(token);
   if (pathname.includes("/api/auth") || token) {
     return NextResponse.next();
   }
